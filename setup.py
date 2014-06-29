c36931a3ecddd96dcf5fbc756cb31c7a5a0c2618
chenhh <chen1116@gmail.com>
2014-06-29 11:58:18 +0800

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pyfv',
+      version='0.1',
+      description='financial time series volatility model toolkit',
+      author='Hung-Hsin Chen',
+      author_email='chenhh@par.cse.nsysu.edu.tw',
+      url='',
+      packages=[],
+     )
