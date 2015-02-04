173813de094bce9450efa3632e485e904230c0f2
chenhh <chenhh@x220>
2015-02-04 21:30:51 +0800

    extreme learning machine source code

--- pyfv/elm/__init__.py
+++ pyfv/elm/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 '''
 extreme learning machine
+https://github.com/dclambert/Python-ELM
 .. codeauthor:: Hung-Hsin Chen <chenhh@par.cse.nsysu.edu.tw>
 '''
 __author__ = 'Hung-Hsin Chen'
