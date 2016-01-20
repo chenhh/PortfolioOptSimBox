c7fdc0708bc367e983c543e98eb22657281185c3
chenhh <chenhh@x220>
2016-01-20 17:03:25 +0800

    project rename

---
+++ post/option/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+"""
+Authors: Hung-Hsin Chen <chenhh@par.cse.nsysu.edu.tw>
+License: GPL v2
+"""
---
+++ post/sampling/cubic_transform.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Authors: Hung-Hsin Chen <chenhh@par.cse.nsysu.edu.tw>
+License: GPL v2
+
+Univariate random variable sampling,
+given target mean, standard deviation, skewness, and kurtosis,
+the algorithm outputs the samples having almost the same statistics.
+"""
