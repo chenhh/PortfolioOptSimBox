f0bc13cc93ce3ed9150532d1982cb98f9cfac5ed
chenhh <chenhh@x220>
2016-04-11 07:37:48 +0800

    update project directory

---
+++ fintechbox/benchmark.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+"""
+Authors: Hung-Hsin Chen <chenhh@par.cse.nsysu.edu.tw>
+License: GPL v2
+"""
+
+import numpy as np
+import pandas as pd
+
+def sharpe(series):
+    """
+    Sharpe ratio
+    note: the numpy std() function is the population estimator
+
+    Parameters:
+    ---------------
+    series: list or numpy.array, ROI series
+    """
+    s = np.asarray(series)
+    try:
+        val = s.mean() / s.std()
+    except FloatingPointError:
+        # set 0 when standard deviation is zero
+        val = 0
+    return val
+
+
+def sortino_full(series, mar=0):
+    """
+    Sortino ratio, using all periods of the series
+
+    Parameters:
+    ---------------
+    series: list or numpy.array, ROI series
+    mar: float, minimum acceptable return, usually set to 0
+    """
+    s = np.asarray(series)
+    mean = s.mean()
+    semi_std = np.sqrt(((s * ((s - mar) < 0)) ** 2).mean())
+    try:
+        val = mean / semi_std
+    except FloatingPointError:
+         # set 0 when semi-standard deviation is zero
+        val = 0
+    return val, semi_std
+
+
+def sortino_partial(series, mar=0):
+    """
+    Sortino ratio, using only negative roi periods of the series
+
+    Parameters:
+    ---------------
+    series: list or numpy.array, ROI series
+    mar: float, minimum acceptable return, usually set to 0
+    """
+    s = np.asarray(series)
+    mean = s.mean()
+    n_neg_period = ((s - mar) < 0).sum()
+    try:
+        semi_std = np.sqrt(((s * ((s - mar) < 0)) ** 2).sum() / n_neg_period)
+        val = mean / semi_std
+    except FloatingPointError:
+        # set 0 when semi-standard deviation or negative period is zero
+        val, semi_std = 0, 0
+    return val, semi_std
+
+
+def maximum_drawdown(series):
+    """
+    https://en.wikipedia.org/wiki/Drawdown_(economics)
+    the peak may be zero
+    e.g.
+    s= [0, -0.4, -0.2, 0.2]
+    peak = [0, 0, 0, 0.2]
+    therefore we don't provide relative percentage of mdd
+
+    Parameters:
+    ---------------
+    series: list or numpy.array, ROI series
+    """
+    s = np.asarray(series)
+    peak = pd.expanding_max(s)
+
+    # absolute drawdown
+    ad = np.maximum(peak - s, 0)
+    mad = np.max(ad)
+
+    return mad
--- setup.py
+++ setup.py
@@ -1,12 +1,48 @@
 #!/usr/bin/env python
+"""
+https://github.com/cython/cython/wiki/InstallingOnWindows
+https://github.com/cython/cython/wiki/CythonExtensionsOnWindows
 
-from distutils.core import setup
+go to start menu >
+    Microsoft Visual C++ Compiler Package for Python 2.7 >
+    Visual C++ 2008 64-bit Command Prompt
 
-setup(name='fintechbox',
-      version='0.1',
-      description='finance technology toolbox',
-      author='Hung-Hsin Chen',
-      author_email='chenhh@par.cse.nsysu.edu.tw',
-      url='',
-      packages=[],
-     )
+Enter the following commands:
+    SET DISTUTILS_USE_SDK=1
+    SET MSSdk=1
+
+You can then "cd X:yourpath" to navigate to your Python app and then
+build your C extensions by entering:
+     python setup.py build_ext --inplace --compiler=msvc
+
+the MSVC compiler default optimization level: /Ox (Full Optimization)
+https://msdn.microsoft.com/en-us/library/59a3b321.aspx
+
+"""
+try:
+    from setuptools import setup
+    from setuptools import Extension
+except ImportError:
+    from distutils.core import setup
+    from distutils.extension import Extension
+
+
+from Cython.Build import cythonize
+import numpy as np
+
+extensions = [
+    Extension(
+              "benchmark",
+              ["benchmark.pyx",],
+              include_dirs = [np.get_include()],
+    ),
+
+]
+
+setup(
+    name = 'fintechbox',
+    description='finance technology toolbox',
+    author = 'Hung-Hsin Chen',
+    author_email = 'chenhh@par.cse.nsysu.edu.tw',
+    ext_modules = cythonize(extensions),
+)
